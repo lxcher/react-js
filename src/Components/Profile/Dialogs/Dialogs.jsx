e7025bce748377a1e55bb9c0479787bd792ea065
lxcher <aleksejcerenkevic@gmail.com>
2022-08-11 23:13:53 +0300

    22

--- src/Components/Profile/Dialogs/Dialogs.jsx
+++ src/Components/Profile/Dialogs/Dialogs.jsx
@@ -1,12 +1,13 @@
 import style from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return <div className={style.dialogs}>
         <div className={style.dialogsItems}>
-            <div className={style.item}>Alex</div>
-            <div className={style.item}>Sveta</div>
-            <div className={style.item}>Max</div>
-            <div className={style.item}>Lena</div>
+            <div className={style.item}><NavLink className={({isActive}) => isActive ? style.active : undefined} to='/dialogs/1'>Alex</NavLink></div>
+            <div className={style.item}><NavLink className={({isActive}) => isActive ? style.active : undefined} to='/dialogs/2'>Sveta</NavLink></div>
+            <div className={style.item}><NavLink className={({isActive}) => isActive ? style.active : undefined} to='/dialogs/3'>Sveta</NavLink></div>
+            <div className={style.item}><NavLink className={({isActive}) => isActive ? style.active : undefined} to='/dialogs/4'>Lena</NavLink></div>
         </div>
         <div className={style.messages}>
             <div className={style.message}>Hi</div>
