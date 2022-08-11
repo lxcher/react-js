a9056550556c234bb3b504b3e6d8cba985239587
lxcher <aleksejcerenkevic@gmail.com>
2022-08-11 22:54:47 +0300

    21

--- src/Components/Profile/Dialogs/Dialogs.jsx
+++ src/Components/Profile/Dialogs/Dialogs.jsx
@@ -1,6 +1,18 @@
+import style from './Dialogs.module.css'
+
 const Dialogs = () => {
-    return <div>
-        Dialogs
+    return <div className={style.dialogs}>
+        <div className={style.dialogsItems}>
+            <div className={style.item}>Alex</div>
+            <div className={style.item}>Sveta</div>
+            <div className={style.item}>Max</div>
+            <div className={style.item}>Lena</div>
+        </div>
+        <div className={style.messages}>
+            <div className={style.message}>Hi</div>
+            <div className={style.message}>How are you?</div>
+            <div className={style.message}>Im fine</div>
+        </div>
     </div>
 }
 
