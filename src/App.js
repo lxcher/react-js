a80f0ee3072fd9a606f368a3eb39b0e21a69621e
lxcher <aleksejcerenkevic@gmail.com>
2022-08-11 21:10:38 +0300

    19

--- src/App.js
+++ src/App.js
@@ -4,18 +4,31 @@ import Navbar from './Components/Navbar/Navbar'
 import Profile from './Components/Profile/Profile'
 import Dialogs from "./Components/Profile/Dialogs/Dialogs";
 import style from "./Components/Profile/Profile.module.css";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+} from "react-router-dom";
+import Settings from "./Components/Profile/Settings/Settings";
+import News from "./Components/Profile/News/News";
 
 function App() {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <Navbar/>
-            <div className='content'>
-                <Profile/>
-                {/*<Dialogs/>*/}
+        <Router>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <div className='content'>
+                    <Routes>
+                        <Route path='/profile' element={<Profile/>}></Route>
+                        <Route path='/dialogs' element={<Dialogs/>}></Route>
+                        <Route path='/news' element={<News/>}></Route>
+                        <Route path='/settings' element={<Settings/>}></Route>
+                    </Routes>
+                </div>
             </div>
+        </Router>
 
-        </div>
     );
 }
 
